4e7d59bf985fede7ef36db9c47a2f59d94593bf7
= <=>
2018-08-16 14:29:04 +0000

    LoadTypes

---
+++ DataFixtures/ORM/LoadElementTypeData.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alexa
+ * Date: 13/06/2018
+ * Time: 12:19
+ */
+
+namespace ScyLabs\NeptuneBundle\DataFixtures\ORM;
+
+
+use ScyLabs\NeptuneBundle\Entity\ElementType;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+
+use Doctrine\Common\Persistence\ObjectManager;
+
+
+class LoadElementTypeData extends Fixture implements OrderedFixtureInterface
+{
+
+    public function load(ObjectManager $manager){
+
+        $types = array();
+        $types[] = new ElementType();
+        $types[0]
+            ->setName('produit')
+            ->setTitle('Produit')
+            ->setRemovable(false);
+        $manager->persist($types[0]);
+
+        $manager->flush();
+
+
+    }
+    public function getOrder(){
+        return 1;
+    }
+}
---
+++ DataFixtures/ORM/LoadZoneTypeData.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alexa
+ * Date: 13/06/2018
+ * Time: 12:19
+ */
+
+namespace ScyLabs\NeptuneBundle\DataFixtures\ORM;
+
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+
+use Doctrine\Common\Persistence\ObjectManager;
+use ScyLabs\NeptuneBundle\Entity\ZoneType;
+
+
+class LoadZoneTypeData extends Fixture implements OrderedFixtureInterface
+{
+
+    public function load(ObjectManager $manager){
+
+        $types = array();
+        $types[] = new ZoneType();
+        $types[0]
+            ->setName('texte')
+            ->setTitle('Texte')
+            ->setRemovable(false);
+        $manager->persist($types[0]);
+
+        $manager->flush();
+
+
+    }
+    public function getOrder(){
+        return 1;
+    }
+}
