291fd69ce19ed4742bc42f125c0d0f7250c7ed21
Alexandre Sciacca <alexandre.sciacca@gmail.com>
2019-02-01 09:43:05 +0100

    Add codeSample

--- Resources/public/admin/js/tinyinit.js
+++ Resources/public/admin/js/tinyinit.js
@@ -4,11 +4,11 @@ tinyMCE.init({
     height: 300,
     menubar: false,
     plugins: [
-        'advlist autolink lists link charmap print preview anchor code textcolor',
+        'advlist autolink lists link charmap print preview anchor code codesample textcolor',
         'searchreplace visualblocks code fullscreen',
         'insertdatetime table contextmenu paste code help wordcount'
     ],
-    toolbar: 'insert | undo redo  | bold italic  | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | code | help',
+    toolbar: 'insert | undo redo  | bold italic  | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | code | codesample | help',
     content_css: [
         '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
         '//www.tinymce.com/css/codepen.min.css']
